bce4bd72570398d9a2ca683cf1655291b79dc497
kunal patil <patilkunnu624@email.com>
2025-07-30 12:50:03 +0530

    index.js

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 
 const corsOptions ={
-    origin:['http://localhost:5173' , "https://job-portal-sepia-five.vercel.app"],
+    origin:['http://localhost:5173' , 'https://job-portal-1-95gk.onrender.com', "https://job-portal-sepia-five.vercel.app"],
     credentials: true
 }
 
