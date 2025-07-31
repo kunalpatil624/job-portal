d4ecdd58b4b646e0813aba56690641d9c4b3a0f7
kunal patil <patilkunnu624@gmail.com>
2025-07-31 12:28:59 +0530

    redux data save problem solving

--- backend/index.js
+++ backend/index.js
@@ -81,6 +81,19 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://job-portal-1-95gk.onrender.com");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  
+  // For preflight requests
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
 
 const corsOptions = {
   origin: function (origin, callback) {
